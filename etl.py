f7a64ae5c35989038c2008239b974b9e34da4c25
Pranjal <pranjal.mishra117@gmail.com>
2025-05-22 22:21:44 -0400

    Updated ETL

--- etl.py
+++ etl.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def load_data(path):
-    return pd.read_csv("raw_data/avocado_ripening.csv")
+    return pd.read_csv("raw_data/avocado_ripeness_dataset.csv")
 
 
 def clean_data(df):
@@ -11,8 +11,8 @@ def clean_data(df):
 
 #def feature_engineering(df)
 
-if __name__ == "main":
-    df = load_data("raw_data/avocado_ripening.csv")
+if __name__ == "__main__":
+    df = load_data("raw_data/avocado_ripeness_dataset.csv")
     df = clean_data(df)
-    df.to_csv("processed_data/avocado_ripening_clean.csv", index =False)
+    df.to_csv("processed_data/avocado_ripeness_clean.csv", index =False)
     print("ETL pipeline completed successfully!")
